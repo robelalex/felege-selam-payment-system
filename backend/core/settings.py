7269bd589621580deeea3d2f30c87730b2c8caad
Robel Alex <robelalex90@.com>
2026-08-27 12:41:13 +0300

    security fixing 2

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -252,7 +252,7 @@ CORS_ALLOW_METHODS      = ['DELETE', 'GET', 'OPTIONS', 'PATCH', 'POST', 'PUT']
 CORS_ALLOW_HEADERS      = [
     'accept', 'accept-encoding', 'authorization', 'content-type',
     'dnt', 'origin', 'user-agent', 'x-csrftoken', 'x-requested-with',
-    'x-school-id',
+    'x-school-id', 'x-reauth-token',
 ]
 
 # ===== PRODUCTION =====
